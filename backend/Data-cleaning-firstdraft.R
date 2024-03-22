873496a1b69d6d928cd41ca22f9d8a0cb1124fc8
jeswid <jessicawidyawati@gmail.com>
2024-03-22 10:20:51 +0800

    second iteration of data cleaning

--- backend/Data-cleaning-firstdraft.R
+++ backend/Data-cleaning-firstdraft.R
@@ -16,12 +16,49 @@ data3 <- read.csv("ResaleFlatPricesBasedonRegistrationDateFromMar2012toDec2014.c
 
 #bind all the data into 1
 
+convert_remaining_lease <- function(data) {
+  # Extract years and months using regular expressions
+  years <- as.numeric(gsub(".*?(\\d+) year.*", "\\1", data$remaining_lease))
+  months <- as.numeric(gsub(".*?(\\d+) month.*", "\\1", data$remaining_lease))
+  
+  # Calculate total lease in years
+  total_lease_years <- years + months / 12
+  
+  # Create separate columns for years and months
+  data$remaining_lease_years <- years
+  data$remaining_lease_months <- months
+  
+  # Convert to numeric
+  data$remaining_lease_years <- as.numeric(data$remaining_lease_years)
+  data$remaining_lease_months <- as.numeric(data$remaining_lease_months)
+  
+  return(data)
+}
+
+# Apply the function to the data
+data5_new <- convert_remaining_lease(data5) %>%
+  mutate(remaining_lease_months = replace_na(remaining_lease_months, 0)) %>%
+  mutate(remaining_lease = remaining_lease_years + remaining_lease_months/12) %>%
+  select(-remaining_lease_months, -remaining_lease_years)
+
+data5 <- data5_new
+
+data1_2_3 <- rbind(data1, data2, data3) %>%mutate(date = ym(month))%>%
+  mutate(lease_commence_date = ym(str_c(lease_commence_date,"01", sep = "-"))) %>%
+  mutate(remaining_lease = 99 - (as.numeric(date - lease_commence_date))/365.25) %>%
+  select(-date) %>%
+  mutate(resale_price_new = resale_price) %>%
+  select(-resale_price) %>%
+  rename(resale_price = resale_price_new)
+
+
 data_original <- rbind.fill(data1, data2, data3, data4, data5)
 
 #create separate year and month columns
-data_transformed <- data_original %>% mutate(date = month) %>%
-  select(-month) %>%
-  separate(date, c("year", "month"), sep = "-")
+data_transformed <- data_original %>% mutate(date = ym(month)) %>%
+  separate(month, c("year", "month"), sep = "-")
+
+
 
 #function for 0-1 recoding:
 convert_to_binary <- function(data, column_name) {
