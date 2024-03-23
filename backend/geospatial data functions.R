765c4ed93d9b1fabff3e8ff456929295d285d8ac
jeswid <jessicawidyawati@gmail.com>
2024-03-23 21:23:28 +0800

    updated hawker_centres postal accuracy

--- backend/geospatial data functions.R
+++ backend/geospatial data functions.R
@@ -74,13 +74,15 @@ supermarkets_geocode = get_lat_long_postal_xy(supermarkets$postal_code)
 hawker_centers = read.csv("backend/ListofGovernmentMarketsHawkerCentres.csv")
 
 #isolate the postal code from the address column of the hawker centre
-hawkers <- hawker_centers %>% mutate(location_of_centre = str_replace(location_of_centre, ", S\\(", "-")) %>%
-  separate(location_of_centre, c("address", "postal"), sep = "-") %>%
+hawkers <- hawker_centers %>% mutate(location_of_centre = str_replace(location_of_centre, ", S\\(", "_")) %>%
+  separate(location_of_centre, c("address", "postal"), sep = "_") %>%
   mutate(postal = str_replace(postal, "\\(", ""))%>%
-  mutate(postal = str_replace(postal, "\\)", ""))
+  mutate(postal = str_replace(postal, "\\)", "")) %>%
+  separate_rows(postal, sep = "/")
+
 
 hawker_centers_geocode = get_lat_long_postal_xy(hawkers$postal)
-# write.csv(hawker_centers_geocode, "backend/hawker_centres_geocode.csv")
+#write.csv(hawker_centers_geocode, "backend/hawker_centres_geocode.csv")
 
 primary_schools = read.csv("backend/primary_schools.csv")
 primary_schools <- primary_schools %>% mutate(Address = str_replace(Address, ", S", "-")) %>%
@@ -91,15 +93,15 @@ primary_schools_geocode = get_lat_long_postal_xy(primary_schools$postal)
 # write.csv(primary_schools_geocode, "backend/primary_schools_geocode.csv")
 ##########################################################################################
 # Test Example of ONEMAP API Call
-# Construct the API request URL
+# #Construct the API request URL
 # base_url <- "https://www.onemap.gov.sg/api/common/elastic/search?searchVal="
 # endpoint <- "&returnGeom=Y&getAddrDetails=Y"
-# resource_url <- paste0(base_url,"069111",endpoint)
+# resource_url <- paste0(base_url,"148812",endpoint)
 # # Make the GET request with the access token in the header
 # res <- GET(resource_url,
 #            add_headers(Authorization = paste("Bearer", Sys.getenv("ONEMAP_KEY"))),
 #            accept("application/json"))
-# 
+# # 
 # # Check the status code
 # res$status_code
 # 
@@ -109,7 +111,7 @@ primary_schools_geocode = get_lat_long_postal_xy(primary_schools$postal)
 # 
 # # Convert response to tibble
 # df <- as_tibble(res_list$results)
-# 
+# df["ADDRESS"]
 # # View the data
 # df
 ##########################################################################################
