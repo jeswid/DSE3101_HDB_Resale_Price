0656c198a843e13534c70bb3e5a2c9535d575be7
lixuan8 <lixuanwong281@gmail.com>
2024-03-30 20:15:51 +0800

    Updated basic side panel for website

--- frontend/server.R
+++ frontend/server.R
@@ -11,18 +11,12 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
+  observeEvent(input$submit, {
+    # This is where you can process the inputs and update the outputs.
+    # For example:
+    output$output <- renderText({
+      paste("You have selected:", input$flat_address, input$town, input$flat_model, input$flat_type, input$floor_area, input$storey, input$lease_commence_date)
     })
-
+  })
 }
+
--- frontend/ui.R
+++ frontend/ui.R
@@ -1,33 +1,55 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+  titlePanel("Interactive App to Visualize and Predict Singapore HDB Resale Prices"),
+  
+  # Sidebar layout with input controls
+  sidebarLayout(
+    sidebarPanel(
+      # Input: Form to collect user inputs
+      textInput("flat_address", "Flat Address or Postal Code", value = "988B BUANGKOK GREEN"),
+      
+      selectInput("town", "Town", 
+                  choices = c('ANG MO KIO', 'BEDOK', 'BISHAN', 'BUKIT BATOK', 'BUKIT MERAH',
+                              'BUKIT TIMAH', 'CENTRAL AREA', 'CHOA CHU KANG', 'CLEMENTI',
+                              'GEYLANG', 'HOUGANG', 'JURONG EAST', 'JURONG WEST',
+                              'KALLANG/WHAMPOA', 'MARINE PARADE', 'QUEENSTOWN', 'SENGKANG',
+                              'SERANGOON', 'TAMPINES', 'TOA PAYOH', 'WOODLANDS', 'YISHUN',
+                              'LIM CHU KANG', 'SEMBAWANG', 'BUKIT PANJANG', 'PASIR RIS','PUNGGOL'),
+                  selected = "SERANGOON"),
+      
+      selectInput("flat_model", "Flat Model", 
+                  choices = c('Model A', 'Improved', 'Premium Apartment', 'Standard',
+                              'New Generation', 'Maisonette', 'Apartment', 'Simplified',
+                              'Model A2', 'DBSS', 'Terrace', 'Adjoined flat', 'Multi Generation',
+                              '2-room', 'Executive Maisonette', 'Type S1S2'),
+                  selected = "Model A"),
+      
+      selectInput("flat_type", "Flat Type", 
+                  choices = c('2 ROOM', '3 ROOM', '4 ROOM', '5 ROOM', 'EXECUTIVE'),
+                  selected = "4 ROOM"),
+      
+      sliderInput("floor_area", "Floor Area (sqm)", min = 34, max = 280, value = 93),
+      
+      selectInput("storey", "Storey", 
+                  choices = c('01 TO 03','04 TO 06','07 TO 09','10 TO 12','13 TO 15',
+                              '16 TO 18','19 TO 21','22 TO 24','25 TO 27','28 TO 30',
+                              '31 TO 33','34 TO 36','37 TO 39','40 TO 42','43 TO 45',
+                              '46 TO 48','49 TO 51'),
+                  selected = "07 TO 09"),
+      
+      selectInput("lease_commence_date", "Lease Commencement Date", 
+                  choices = as.character(rev(1966:2016)), 
+                  selected = "1967"),
+      
+      actionButton("submit", "Submit HDB 🔎")
+    ),
+    
+    # Main panel for displaying outputs
+    mainPanel(
+      textOutput("output")
+      # Placeholders for other UI elements like plots, tables, etc.
     )
+  )
 )
+
