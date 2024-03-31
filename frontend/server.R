01b20daf8cdc1c2c8e757c6c85eeda1acc6ca0c2
wantingkwek <wantingkwek@gmail.com>
2024-04-01 00:04:10 +0800

    added the map layer

--- frontend/server.R
+++ frontend/server.R
@@ -1,8 +1,55 @@
-
 library(shiny)
+library(dplyr)
+require(pals)
+library(RColorBrewer)
+
+
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
+  shinyjs::addClass(selector = "body", class = "sidebar-collapse")
+  ######################## REACTIVE FUNCTION ###########################################################
+  filteredData <- reactive({
+    all_address %>%
+      filter(mrt == all_address$`mrt_1km`, 
+             hawkers == alladress$`hawkers_1km`, 
+             schs == alladdress$`primary_schools_1km`, 
+             hospitals == alladress$`hospitals_1km`,
+             town == alladdress$street
+             )
+    })
+  
+  colorpal <- reactive({
+  colorNumeric(input$colors, median_resale_prices$median_price)
+  })
+  
+  filteredhawkers <- reactive({
+    hawker_centres %>% 
+      filter(., Year == input$yr, ENAME == input$dist_id)
+  })
+  
+  filteredhosp <- reactive({
+    hospitals %>% 
+      filter(., Year == input$yr, ENAME == input$dist_id)
+  })
+  
+
+  ################################## M A P   L A Y E R ########################################################
+  # Create the map
+  output$map <- renderLeaflet({
+    leaflet(SG_map) %>%
+      addTiles() %>%
+      addProviderTiles('OpenStreetMap') %>% 
+      addPolygons(weight = 1, smoothFactor = 0.5,
+                  opacity = 0, fillOpacity = 0,
+                  highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                      bringToFront = TRUE),
+                  popup = paste(sep = "<br/>",
+                                paste0(all_address$town),
+                                round(median_resale_prices$median_price)))
+  })
+
+  
   observeEvent(input$submit, {
     # This is where you can process the inputs and update the outputs.
     # For example:
@@ -14,8 +61,6 @@ function(input, output, session) {
   output$geoOutput <- renderText({ "Map will be displayed here." })
   
   output$priceOutput <- renderText({ "Predicted price will be shown here." })
-  
-
   
 }
 
