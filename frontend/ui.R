88f28243a54e7064a3b270994a253a4ae411c766
lixuan8 <lixuanwong281@gmail.com>
2024-04-02 15:40:01 +0800

    Modified webite

--- frontend/ui.R
+++ frontend/ui.R
@@ -51,26 +51,29 @@ ui <- fluidPage(
                                       selected = "Model A"),
                           selectInput("flat_type", "Flat Type", choices = c('2 ROOM', '3 ROOM', '4 ROOM', '5 ROOM', 'EXECUTIVE'), selected = "4 ROOM"),
                           selectInput("amenities", "Amenities", choices = c("Primary School", "Shopping Centre", "Food Court", "Gym", "Community Center", "Junior College"), selected = "Primary School"),
-                          actionButton("submit", "Submit HDB 🔎", class = "btn-primary")
-                      )
-               ),
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                      )
-               )
-             ),
-
+                          actionButton("submit", "Submit HDB 🔎", class = "btn-primary") ) ), ) ),
     tabPanel("Predicted Price", value = "PredictedPrice",
              fluidRow(
                column(12,
                       # Content for Predicted Price tab
-                      textOutput("priceOutput") ) )))
+                      textOutput("priceOutput") )),
+             div(id = "sidebar", class = "well",
+                          selectInput("town", "Town", choices = c('ANG MO KIO', 'BEDOK', 'BISHAN', 'BUKIT BATOK', 'BUKIT MERAH',
+                                                                  'BUKIT TIMAH', 'CENTRAL AREA', 'CHOA CHU KANG', 'CLEMENTI',
+                                                                  'GEYLANG', 'HOUGANG', 'JURONG EAST', 'JURONG WEST',
+                                                                  'KALLANG/WHAMPOA', 'MARINE PARADE', 'QUEENSTOWN', 'SENGKANG',
+                                                                  'SERANGOON', 'TAMPINES', 'TOA PAYOH', 'WOODLANDS', 'YISHUN',
+                                                                  'LIM CHU KANG', 'SEMBAWANG', 'BUKIT PANJANG', 'PASIR RIS', 'PUNGGOL'), 
+                                      selected = "SERANGOON"),
+                          selectInput("flat_model", "Flat Model", choices = c('Model A', 'Improved', 'Premium Apartment', 'Standard',
+                                                                              'New Generation', 'Maisonette', 'Apartment', 'Simplified',
+                                                                              'Model A2', 'DBSS', 'Terrace', 'Adjoined flat', 'Multi Generation',
+                                                                              '2-room', 'Executive Maisonette', 'Type S1S2'), 
+                                      selected = "Model A"),
+                          selectInput("flat_type", "Flat Type", choices = c('2 ROOM', '3 ROOM', '4 ROOM', '5 ROOM', 'EXECUTIVE'), selected = "4 ROOM"),
+                          selectInput("amenities", "Amenities", choices = c("Primary School", "Shopping Centre", "Food Court", "Gym", "Community Center", "Junior College"), selected = "Primary School"),
+                          actionButton("submit", "Submit HDB 🔎", class = "btn-primary") )), ))
+
   
 
 
