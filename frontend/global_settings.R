fec722f9a2943262d74a783cc5fe342940e90200
wantingkwek <wantingkwek@gmail.com>
2024-04-03 10:06:16 +0800

    edited csv

--- frontend/global_settings.R
+++ frontend/global_settings.R
@@ -17,7 +17,6 @@ SG_map = readRDS("data/SG_map.rds")
 all_address = read.csv("data/hdb_block_details.csv") %>%
   rename(lng = long) %>%
   rename("Primary School" = "primary_schools_1km")
-all_address_sf = st_as_sf(all_address, coords = c("lng","lat"))
 amenities = read.csv("data/lat_long_for_visualisation.csv")
 hawker_centres = read.csv("data/hawker_centres_geocode.csv")
 hospitals = read.csv("data/hospitals_geocode.csv")
